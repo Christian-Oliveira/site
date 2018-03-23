3c191ce0ba3536cc5e87b433854ffe81fce290d9
Christian Oliveira <christianoliveirati@gmail.com>
2018-03-23 11:55:22 -0300

    formulario simples das questions e escolhas

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	#/polls/
     url(r'^$', views.index, name='index'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -17,4 +17,15 @@ def results(resquest, question_id):
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." %question_id)
+	question = get_object_or_404(Question, pk = question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
