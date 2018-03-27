00bc62a35162fbe3ffe8d3894141847e04b546f2
Christian Oliveira <christianoliveirati@gmail.com>
2018-03-27 11:53:26 -0300

    testes automatizados

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question (models.Model):
@@ -13,3 +15,7 @@ class Choice (models.Model):
 	votes = models.IntegerField(default=0)
 	def __str__(self):
 		return self.choice_text
+
+def was_published_recently(self):
+	now = timezone.now()
+	return now - datetime.timedelta(days=1) <= self.pub_date <= now
